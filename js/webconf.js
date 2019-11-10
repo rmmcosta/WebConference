c06963b230c4d7740f509d1448de6e4c64ac288a
rmmcosta <ricardocosta101085@gmail.com>
2019-11-10 19:08:52 +0000

    added about and speakers sections and data. Popup for the speakers missing.

--- js/webconf.js
+++ js/webconf.js
@@ -1,13 +1,17 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "https://rcwebconference.herokuapp.com/"; // site that doesn’t send Access-Control-*
-const baseApiUrl = proxyurl+url;
+const proxyurl = "https://cors-anywhere.herokuapp.com";
+const url = "https://rcwebconference.herokuapp.com"; // site that doesn’t send Access-Control-*
+const baseApiUrl = proxyurl + '/' + url;
 const conferenceNumber = 1;
 
 let jsonHeaders = new Headers();
 jsonHeaders.append('Content-Type', 'application/json');
 
+
+//on dom loaded
 window.onload = function () {
     console.log("DOM loaded");
+
+    //register participant
     const btnRegister = document.getElementById("btnRegister");
     btnRegisterClick = function () {
         console.log("Register button clicked!")
@@ -35,17 +39,18 @@ window.onload = function () {
                     headers: jsonHeaders,
                     mode: 'cors',
                     cache: 'default',
-                    body: JSON.stringify({ name:inname, email:inmail})
+                    body: JSON.stringify({ name: inname })
                 };
                 console.log('do the fetch at:' + new Date().getTime());
-                return fetch(`${baseApiUrl}participants`, postInit)
-                    .then(response => { 
-                        console.log(response);
-                        if (!response.ok) {
+                return fetch(`${baseApiUrl}/conferences/${conferenceNumber}/participants/${inmail}`, postInit)
+                    .then(response => response.text())
+                    .then(data => {
+                        console.log(data);
+                        if (!data) {
                             console.log('response not ok');
                             throw new Error(response.statusText)
                         }
-                        return true;
+                        return data;
                     })
                     .catch(error => {
                         console.log('error');
@@ -59,7 +64,7 @@ window.onload = function () {
             if (result.value) {
                 if (!result.value.err_code) {
                     Swal.fire({
-                        title: `Registered with success!`
+                        title: result.value
                     })
 
                 } else {
@@ -71,4 +76,98 @@ window.onload = function () {
         })
     }
     btnRegister.addEventListener("click", btnRegisterClick);
+
+    (async () => {
+        try {
+            const speakers = await getSpeakers(`${baseApiUrl}/conferences/${conferenceNumber}/speakers`);
+            console.log(JSON.stringify(speakers)); // JSON-string from `response.json()` call= 
+            let renderSpeakers = document.getElementById('renderSpeakers');
+            renderSpeakers.innerHTML = getSpeakersHtml(speakers);
+            await addSpeakerPhotos();
+        } catch (error) {
+            console.error(error);
+        }
+
+        async function getSpeakers(url = '') {
+            // Default options are marked with *
+            const response = await fetch(url, {
+                method: 'GET', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'application/json'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrer: 'no-referrer', // no-referrer, *client
+            });
+            return await response.json(); // parses JSON response into native JavaScript objects
+        }
+
+        function getSpeakersHtml(speakers) {
+            let htmlSpeakers = '<div class="row text-center">';
+            for (let speaker in speakers) {
+                let name = speakers[speaker].name;
+                let bio = speakers[speaker].bio;
+                htmlSpeakers = htmlSpeakers + '<div class="col-sm-4">';
+                htmlSpeakers = htmlSpeakers + '<img class="mx-auto rounded-circle" src="./img/spinner.gif" style="width:150px;height:150px;" alt=""></img>';
+                htmlSpeakers = htmlSpeakers + `<h4 class="speaker-name">${name}</h4>`;
+                htmlSpeakers = htmlSpeakers + `<p class="text-muted">${bio}</p>`;
+                htmlSpeakers = htmlSpeakers + '<ul class="list-inline social-buttons">';
+                htmlSpeakers = htmlSpeakers + '<li class="list-inline-item"><a href="#"><i class="fab fa-twitter"></i></a></li>';
+                htmlSpeakers = htmlSpeakers + '<li class="list-inline-item"><a href="#"><i class="fab fa-facebook"></i></a></li>';
+                htmlSpeakers = htmlSpeakers + '<li class="list-inline-item"><a href="#"><i class="fab fa-linkedin-in"></i></a></li>';
+                htmlSpeakers = htmlSpeakers + '</ul>';
+                htmlSpeakers = htmlSpeakers + '</div>';
+            }
+            htmlSpeakers = htmlSpeakers + '</div>';
+            console.log(htmlSpeakers);
+            return htmlSpeakers;
+        }
+
+    })();
 }
+//end of on dom loaded function
+
+async function addSpeakerPhotos() {
+    console.log('add speaker photos');
+    const imgs = document.querySelectorAll("#renderSpeakers .row img");
+    console.log(imgs);
+    for (img in imgs) {
+        await getRandomPhoto(imgs[img]);
+    };
+}
+
+async function getRandomPhoto(img) {
+    //console.log('get random photo');
+    //console.log(img);
+    fetch('https://picsum.photos/150/150')
+        .then(response => response.blob())
+        .then(function (imgblob) {
+            //console.log(imgblob);
+            let objectURL = URL.createObjectURL(imgblob);
+            img.src = objectURL;
+        });
+}
+
+/*
+<div id="image-holder"></div>
+<script>
+  function loadImage(url) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.addEventListener('load', e => resolve(img));
+      img.addEventListener('error', () => {
+        reject(new Error(`Failed to load image's URL: ${url}`));
+      });
+      img.src = url;
+    });
+  }
+
+  // load the image, and append it to the element id="image-holder"
+  loadImage('http://thecatapi.com/api/images/get?format=src&type=jpg&size=small')
+    .then(img => document.getElementById('image-holder').appendChild(img))
+    .catch(error => console.error(error));
+
+</script>*/
